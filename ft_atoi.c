c12d7ff52268bb6bcb831d34df8f03bd1a4a7313
pestelle <pepestellediaz@gmail.com>
2025-01-15 12:16:39 +0100

    atoi

---
+++ ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pestell2 <pestell2@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 10:15:03 by pestell2          #+#    #+#             */
+/*   Updated: 2025/01/15 12:11:16 by pestell2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_atoi(const char *str)
+{
+	int	res;
+	int	sign;
+
+	res = 0;
+	sign = 1;
+	while (*str == ' ' || *str == '\n' || *str == '\t')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		else
+			sign = 1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - '0');
+		str++;
+	}
+	return (res * sign);
+}
+int	main(void)
+{
+    const char* str = "12345";
+    int num = ft_atoi(str);
+    printf("Converted integer: %d\n", num);
+    return (0);
+}
